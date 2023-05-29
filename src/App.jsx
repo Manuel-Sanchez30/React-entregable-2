b99187418cae8e6edf36fcf40db50a0f9816a500
Manuel <milton.sanchezrios88@gmail.com>
2023-05-29 10:45:50 -0500

    proyecto version1

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+
+import { useEffect, useState } from 'react'
+import './App.css'
+import axios from 'axios'
+import getApiKey from './utils/getApiKy'
+import WeatherCard from './components/WeatherCard'
+import Loading from './components/Loading'
+
+
+function App() {
+
+const [coords, setCoords] = useState()
+const [weather, setWeather] = useState()
+const [temp, setTemp] = useState()
+
+
+useEffect(() => {
+  const success = pos => {
+    console.log(pos)
+    const obj = {
+      lat: pos.coords.latitude,
+      lon: pos.coords.longitude
+    }
+  
+    setCoords(obj)
+  }
+  
+    navigator.geolocation.getCurrentPosition(success)
+}, [])
+
+useEffect(() => {
+  if(coords){
+      const URL = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${getApiKey()}`
+    axios.get(URL)
+      .then(res => {
+        setWeather(res.data)
+        const objTemp = {
+          celsius: +(res.data.main.temp - 273.15).toFixed(1),
+          farenheit: +((res.data.main.temp - 273.15) * 9/5 +32).toFixed(1),
+        }
+        setTemp(objTemp)
+      })
+      .catch(err => console.error(err))
+  }
+  
+
+  
+}, [coords])
+
+
+
+    return (
+      <div className="app snow">
+        {
+          weather
+          ? <WeatherCard weather={weather}
+            temp={temp}
+            />
+          : <Loading/>
+
+        }
+        
+
+      </div>
+    )
+}
+
+export default App
---
+++ src/utils/getApiKy.js
@@ -0,0 +1,3 @@
+const getApiKey = () => "a086c5c9d666d491d2e32e009f7f1a55"
+
+export default getApiKey
